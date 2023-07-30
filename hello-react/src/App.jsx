28dd6d8ebcce8fe68c66c71ba88acb583d8f89ea
prathibha <prathibha@LAPTOP-NAAMUCTV>
2023-07-30 21:31:23 +0530

    css styles added

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -2,17 +2,16 @@ import TaskCard from "./TaskCard"
 import './TaskCard.css'
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen">
-        <div className="flex justify-center mt-10">
+    <div >
+      <div className="flex justify-center h-screen mt-10">
         <div className="TaskItem w-1/2 p-4 mx-2">
             <h1>Pending</h1>
             <div>
             <TaskCard title= "Build the website with static content" dueDate= "10th April" assigneeName= "Rohit S"/>
             </div>
             <div>
-            <TaskCard title= "Add blog" dueDate= "22nd March" assigneeName= "Rohit M"/>
-            </div>
-            
+            <TaskCard title= "Add a blog" dueDate= "22nd March" assigneeName= "Rohit M"/>
+          </div>
         </div>
         <div className="TaskItem w-1/2 p-4 mx-2">
             <h1>Done</h1>
@@ -20,12 +19,12 @@ function App() {
             <TaskCard title= "Design the mockup" completedAtDate= "10th April" assigneeName= "Rohit M"/>
             </div>
             <div>
-            <TaskCard title= "Get principal approval" completedAtDate= "20th April" assigneeName= "Ajay S"/>
+            <TaskCard title= "Get the approval from principal" completedAtDate= "20th April" assigneeName= "Ajay S"/>
             </div>
             
         </div>
-        </div>
       </div>
+    </div>
   )
 }
 
