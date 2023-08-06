8a620ddee860970d497660fc5075d87be29d4fe1
prathibha <prathibha@LAPTOP-NAAMUCTV>
2023-08-06 13:55:32 +0530

    updated to hooks

--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -1,25 +1,23 @@
-import React from "react";
+
 import "./TaskCard.css";
-interface TaskProp {
+interface TaskItem {
     title: string;
     description: string;
     dueDate: string
   }
-  class Task extends React.Component<TaskProp> {
-    render() {
-        return (
-          <div className="TaskItem shadow-md border border-slate-100">
-            <h2 className="text-base font-bold my-1">Title: {this.props.title}</h2>
-            
-            <p className="text-sm text-slate-500">
-              Due Date: {this.props.dueDate}
-            </p>
-            <p className="text-sm text-slate-500">
-              Description: {this.props.description}
-            </p>
-          </div>
-        );
-      }
-    
-  }
+  const Task = (props: TaskItem) => {
+    return (
+      <div className="TaskItem shadow-md border border-slate-100">
+        <h2 className="text-base font-bold my-1">{props.title}</h2>
+        <p className="text-sm text-slate-500">{props.dueDate}</p>
+        <p className="text-sm text-slate-500">Description: {props.description}</p>
+      </div>
+    );
+  };
+
+
+
+
+
+  
 export default Task;
--- smarter-tasks/src/TaskList.tsx
+++ smarter-tasks/src/TaskList.tsx
@@ -1,16 +1,18 @@
-import React from "react";
 import Task from "./Task";
 import { TaskItem } from "./types";
 interface Props {
   tasks: TaskItem[];
 }
-interface State {}
-class TaskList extends React.Component<Props, State> {
-  
-  render() {
-    return this.props.tasks.map((task, idx) => (
-      <Task key={idx} title={task.title} description={task.description} dueDate={task.dueDate}/>
-    ));
-  }
-}
+
+const TaskList = (props: Props) => {
+  const list = props.tasks.map((task, idx) => (
+    <Task
+      key={idx}
+      title={task.title}
+      description={task.description}
+      dueDate={task.dueDate}
+    />
+  ));
+  return <>{list}</>;
+};
 export default TaskList;
