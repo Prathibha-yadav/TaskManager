21a19953a7c042931c488d694a78e33a4421425d
prathibha <prathibha@LAPTOP-NAAMUCTV>
2023-08-06 21:26:54 +0530

    added homePage and tasklistpage

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,9 +1,33 @@
-import TaskApp from "./TaskApp";
-function App() {
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from './pages/HomePage';
+import TaskListPage from './pages/TaskListPage';
+//import TaskDetailsPage from "./pages/TaskDetailsPage";
+import Layout from "./Layout";
+
+const router = createBrowserRouter([
+  {
+    element: (
+      <Layout />
+    ),
+    children: [
+      {
+        path: "/home",
+        element: (<HomePage />)
+      },
+      {
+        path: "tasks",
+        element: (<TaskListPage />)
+      }
+    ]
+  }
+]);
+const App = () => {
   return (
-    <div className="App">
-      <TaskApp />
-    </div>
+    <RouterProvider router={router} />
   );
 }
-export default App;
+
+export default App
