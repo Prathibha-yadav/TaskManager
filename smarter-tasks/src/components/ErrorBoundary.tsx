d46cd9a1e9ec0a2231ae728f118adef500a890c5
prathibha <prathibha@LAPTOP-NAAMUCTV>
2023-10-07 20:23:19 +0530

    lev 9milestone

--- smarter-tasks/src/components/ErrorBoundary.tsx
+++ smarter-tasks/src/components/ErrorBoundary.tsx
@@ -15,7 +15,7 @@ class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
     this.state = { hasError: false };
   }
 
-  static getDerivedStateFromError(): ErrorBoundaryState {
+  static getDerivedStateFromError(error: Error): ErrorBoundaryState {
     return { hasError: true };
   }
 
